9979f82655606fcf15ad24eca302c0e3a6fd7778
Noah Taggart <noahtag@gmail.com>
2022-01-11 11:36:51 -0600

    began js

---
+++ scripts/database.js
@@ -0,0 +1,13 @@
+const database = {
+    fish: [
+        {
+            name: "Bart",
+            food: "crustaceans",
+            size: 3
+        }
+    ]
+}
+
+export const getFish = () => {
+    return database.fish.map(fish => ({...fish}))
+}
